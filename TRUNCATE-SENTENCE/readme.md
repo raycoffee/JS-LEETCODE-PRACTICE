0750f5f071f2eac1391b546c004a850debda1bf6
Dhruvjyoti Ray <dhruvjyotiray@gmail.com>
2022-11-14 10:17:54 +0530

    JS solution leetcode

---
+++ TRUNCATE-SENTENCE/readme.md
@@ -0,0 +1,1 @@
+![screenshot](performance.png)
---
+++ TRUNCATE-SENTENCE/solution.js
@@ -0,0 +1,17 @@
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+
+// For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+// You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+
+// Example 1:
+
+// Input: s = "What is the solution to this problem", k = 4
+// Output: "What is the solution"
+// Explanation:
+// The words in s are ["What", "is" "the", "solution", "to", "this", "problem"].
+// The first 4 words are ["What", "is", "the", "solution"].
+// Hence, you should return "What is the solution".
+
+let truncateSentence = function (s, k) {
+  return s.split(" ").slice(0, k).join(" ");
+};
