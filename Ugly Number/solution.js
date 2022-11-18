80ced879fdebe4afab7b3ba3a065d9ceb9316b51
Dhruvjyoti Ray <dhrubojyoti21@gmail.com>
2022-11-18 22:08:37 +0530

    JS solution

---
+++ Ugly Number/solution.js
@@ -0,0 +1,33 @@
+// An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+// Given an integer n, return true if n is an ugly number.
+
+// Example:
+
+// Input: n = 6
+// Output: true
+// Explanation: 6 = 2 × 3
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function (n) {
+  let ugly = false;
+  while (ugly == false) {
+    if (n == 0) {
+      return false;
+    }
+    if (n % 2 == 0) {
+      n /= 2;
+    } else if (n % 3 == 0) {
+      n /= 3;
+    } else if (n % 5 == 0) {
+      n /= 5;
+    } else {
+      if (n == 1) {
+        return true;
+      } else return false;
+    }
+  }
+};
