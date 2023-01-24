41c6eee4387b10e6ac9a3cd7d8c93c37ca4a12ff
Dhruvjyoti Ray <dhrubojyoti21@gmail.com>
2023-01-24 21:42:19 +0530

    Leetcode solution

---
+++ Kth Smallest Element in a BST/solution.js
@@ -0,0 +1,33 @@
+// Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+// Example 1:
+
+// Input: root = [3,1,4,null,2], k = 1
+// Output: 1
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k) {
+  let arr = [];
+
+  function dfs(node) {
+    if (node.left) dfs(node.left);
+    arr.push(node.val);
+    if (node.right) dfs(node.right);
+    return;
+  }
+
+  dfs(root);
+  return arr[k - 1];
+};
