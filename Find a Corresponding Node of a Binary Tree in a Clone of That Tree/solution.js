de48807c51dd91ba5aae4943b337bd2526c5ba3e
Dhruvjyoti Ray <dhrubojyoti21@gmail.com>
2023-02-03 19:52:55 +0530

    Leetcode medium

---
+++ Find a Corresponding Node of a Binary Tree in a Clone of That Tree/solution.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
+
+var getTargetCopy = function (original, cloned, target) {
+  let stack = [cloned];
+
+  while (stack.length > 0) {
+    let currNode = stack.shift();
+    if (currNode.val == target.val) return currNode;
+    if (currNode.left) stack.push(currNode.left);
+    if (currNode.right) stack.push(currNode.right);
+  }
+};
