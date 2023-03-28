91dd955e5a15aac1139317e6f81eff71e5b65b47
Dhruvjyoti Ray <dhrubojyoti21@gmail.com>
2023-03-28 21:32:39 +0530

    Leetcode solutio

---
+++ Count Asterisks/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countAsterisks = function (s) {
+  let count = 0;
+  let firstEn = false;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "|") {
+      if (!firstEn) firstEn = true;
+      else firstEn = false;
+    }
+
+    if (s[i] === "*") {
+      if (firstEn) continue;
+      else count++;
+    }
+  }
+
+  return count;
+};
