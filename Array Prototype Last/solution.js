fe2b41be8ded05e56cc7a31e39ebe212bd99aabb
Dhruvjyoti Ray <dhrubojyoti21@gmail.com>
2023-04-12 20:31:35 +0530

    Leetcode solution

---
+++ Array Prototype Last/solution.js
@@ -0,0 +1,12 @@
+Array.prototype.last = function() {
+
+    let length = this.length
+    if (length === 0) return -1
+
+    return this[length-1]
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
