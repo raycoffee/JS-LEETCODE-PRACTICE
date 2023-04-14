2b97e7b15888e21eebdeb5bf62385994ebf8b88a
Dhruvjyoti Ray <dhrubojyoti21@gmail.com>
2023-04-14 21:33:39 +0530

    Leetcode solution

---
+++ Array Reduce Transformation/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function (nums, fn, init) {
+  res = init;
+
+  for (let num of nums) {
+    res = fn(res, num);
+  }
+
+  return res;
+};
