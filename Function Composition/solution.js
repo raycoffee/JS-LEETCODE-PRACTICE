f976cc855bf8d24c67b0e9f48a495e48c617ee19
Dhruvjyoti Ray <dhrubojyoti21@gmail.com>
2023-05-26 21:37:29 +0530

    Leetcode solution

---
+++ Function Composition/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function (functions) {
+    return function (x) {
+
+        return functions.reduceRight((a, b) => {
+            let sum = b(a)
+            return sum
+        }, x)
+
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
